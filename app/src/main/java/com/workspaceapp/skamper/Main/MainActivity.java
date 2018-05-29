357c985e2938ec8574629f9c121821ccdf0dd3c8
Sebastian <sebs9302@gmail.com>
2018-05-29 19:47:46 +0200

    Added catalogs Login and Main on app sources

---
+++ app/src/main/java/com/workspaceapp/skamper/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.workspaceapp.skamper.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.workspaceapp.skamper.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
