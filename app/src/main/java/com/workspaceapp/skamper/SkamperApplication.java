b244ef12bd10438d04096b4f0fb28f9f8b045471
Sebastian <sebs9302@gmail.com>
2018-05-29 20:31:14 +0200

    Changes on strings.xml file

--- app/src/main/java/com/workspaceapp/skamper/SkamperApplication.java
+++ app/src/main/java/com/workspaceapp/skamper/SkamperApplication.java
@@ -3,4 +3,8 @@ package com.workspaceapp.skamper;
 import android.app.Application;
 
 public class SkamperApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
