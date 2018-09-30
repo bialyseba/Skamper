be62e652b3d2412d5022e39a9216d04fd5c9d4f5
WojciechKwasnyy <wojtekkwa@o2.pl>
2018-09-30 18:18:10 +0200

    Call to recipient is working

--- app/src/main/java/com/workspaceapp/skamper/main/MainActivity.java
+++ app/src/main/java/com/workspaceapp/skamper/main/MainActivity.java
@@ -2,15 +2,21 @@ package com.workspaceapp.skamper.main;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.sinch.android.rtc.Sinch;
+import com.sinch.android.rtc.SinchClient;
+import com.sinch.android.rtc.calling.Call;
 import com.workspaceapp.skamper.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Call call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,24 @@ public class MainActivity extends AppCompatActivity {
         String username = user.getEmail();
         TextView textView = findViewById(R.id.testTextview);
         textView.setText(username);
+        Button button = (Button) findViewById(R.id.button);
+        SinchClient sinchClient = Sinch.getSinchClientBuilder()
+                .context(this)
+                .userId(user.getEmail())
+                .applicationKey("ae90c73b-4fcd-4a40-8a5b-ab8a0adb642a")
+                .applicationSecret("0li5kxQXtUyF+D9ruk7MHg==")
+                .environmentHost("sandbox.sinch.com")
+                .build();
+        sinchClient.setSupportCalling(true);
+        sinchClient.start();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sinchClient.getCallClient().callUser("call-recipient-id");
+                //TODO dodac nazwe usera koncowego
+            }
+        });
+
     }
 
     public static Intent getStartIntent(Context context) {
